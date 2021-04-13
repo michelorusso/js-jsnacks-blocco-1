8046c0dfe73f18425b311c844439a41b2c6a7eb6
Michele Lorusso <kirito102095@hotmail.it>
2021-04-13 14:36:53 +0200

    Consegna Esercizio

--- js/script.js
+++ js/script.js
@@ -53,22 +53,64 @@ var arrayZucchine = [
         'peso': 0,
         'lunghezza': 0
     },
-];
+]
+
+var somma = 0;
+
+
+var small = [];
+var large = [];
+
 
 for( var i = 0; i < arrayZucchine.length; i++ ) {
     var thisZucchina = arrayZucchine[i];
-    console.log(team);
+    // console.log(thisZucchina);
 
     thisZucchina.peso = getRndInteger(1 , 10);
     thisZucchina.lunghezza = getRndInteger(1 , 30);
 
+    var peso = thisZucchina.peso;
+    var lunghezza = thisZucchina.lunghezza;
+    
+    // Calcola la somma del peso di tutte le zucchine.
+    somma += peso;
+
+    // Dividi in due array separati le zucchine che misurano meno o più di 15cm. 
+    if( lunghezza <= 15 ) {
+        small.push(thisZucchina);
+    } else if ( lunghezza > 15 ) {
+        large.push(thisZucchina);
+    }
+
 }
 
+
 console.log(arrayZucchine);
-// Calcola la somma del peso di tutte le zucchine.
-// Crea 10 oggetti che rappresentano una zucchina.
-// Dividi in due array separati le zucchine che misurano meno o più di 15cm. 
+console.log('Il peso complessivo di tutte le zucchine è di: ', somma , 'Kg');
+
+console.log('Le zucchine più piccole sono: ', small);
+console.log('Le zucchine più lunghe sono: ', large);
+
 // Infine stampa separatamente quanto pesano i due gruppi di zucchine.
+var sommaSmall = 0;
+for( var j = 0; j < small.length; j++) {
+    var thisZucchinaSmall = small[j];
+
+    sommaSmall += thisZucchinaSmall.peso;
+
+}
+
+var sommaLarge = 0;
+for( var z = 0; z < large.length; z++) {
+    var thisZucchinaLarge = large[z];
+
+    sommaLarge += thisZucchinaLarge.peso;
+
+}
+
+console.log('Peso complessivo Zucchine piccole: ', sommaSmall);
+console.log('Peso complessivo Zucchine lunghe: ', sommaLarge);
+
 
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min) ) + min;
